94a567f2de121fbc3de3e2aff0c56ce31024cd48
jhnlem <jhnlem98@gmail.com>
2024-10-17 16:13:16 +0300

    done

---
+++ n_decimal_places_of_pi.c
@@ -0,0 +1,26 @@
+/*
+we will use the Leibniz formula for pi:
+pi/4 = sum_k=0( (-1)^k / (2k + 1) ) 
+(it's inefficient)
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+	double s = 0, ps = 0, k = 0;
+	double n = 5;
+
+	do {
+		ps = s;
+		s += (4 * pow(-1,k)) / (2 * k + 1);
+		k++;
+	} while(fabs(ps - s) > pow(10,-n-1));
+
+	printf("%.5f\n", s);
+
+
+
+
+	return 0;
+}
