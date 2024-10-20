0df37ad68b74e374c67cbbf374a36c1d076504e0
jhnlem <jhnlem98@gmail.com>
2024-10-20 17:26:37 +0300

    update sin_derivative.c

--- sin_derivative.c
+++ sin_derivative.c
@@ -1,32 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
-
-
 double F(double x) {
 	return sin(x);
 }
 
-
 double f(double x) {
 	double h = 0.0001;
 	return (F(x + h) - F(x))/h;
-
-
-
 }
 
-
-
-
 int main(void) {
-
 	double a = f(3.14);
-
 	printf("%f\n",a);
-
-
-
-
 	return 0;
 }
