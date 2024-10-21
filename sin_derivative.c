f2ab7f9a435ca9b532ad4bb8e8309786e5338178
jhnlem <jhnlem98@gmail.com>
2024-10-21 20:55:09 +0300

    doc sin_derivative.c

--- sin_derivative.c
+++ sin_derivative.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
+//function
 double F(double x) {
 	return sin(x);
 }
 
+//derivative
 double f(double x) {
 	double h = 0.0001;
 	return (F(x + h) - F(x))/h;
