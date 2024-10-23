eece5ee274460033c0e7fe455426d38bdbabca9a
jhnlem <jhnlem98@gmail.com>
2024-10-23 19:41:22 +0300

    update recursive_merge_sort.c

--- recursive_merge_sort.c
+++ recursive_merge_sort.c
@@ -3,19 +3,20 @@
 /*
  * Split the array V in half and merge the sub arrays.
  */
-void merge(int first, int last, double V[])
+void merge(size_t first, size_t last, double V[])
 {
-	// Position of the first element of the left sub array.
-	int i = first;
-	// Position of the first element of the right sub array.
-	int j = (first + last) / 2 + 1;
-	// Size of array V.
-	int size = last - first + 1;
-	int c = 0;
-	double U[size];
+	size_t llast = (first + last) / 2;
+	size_t rfirst = llast + 1;
+	
+	size_t sizeV = last - first + 1;
+	double U[sizeV];
+
+	size_t i = first;
+	size_t j = rfirst;
+	size_t c = 0;
 
 	// Merge the sub arrays until one of them ends.
-	while (i <= (first + last) / 2 && j <= last) {
+	while (i <= llast && j <= last) {
 		if (V[i] <= V[j]) {
 			U[c] = V[i];	
 			i++;
@@ -27,7 +28,7 @@ void merge(int first, int last, double V[])
 	}
 
 	// Append the remaining.
-	while (i <= (first + last) / 2) {
+	while (i <= llast) {
 		U[c] = V[i];
 		i++;
 		c++;
@@ -38,14 +39,14 @@ void merge(int first, int last, double V[])
 		c++;
 	}
 
-	for (int t = first; t <= last; ++t)
+	for (size_t t = first; t <= last; ++t)
 		V[t] = U[t - first];
 }
 
 /*
  * Recursive mergesort.
  */
-void rmsort(int first, int last, double V[])
+void rmsort(size_t first, size_t last, double V[])
 {
 	if (first < last) {
 		rmsort(first, (last + first) / 2, V);
@@ -57,7 +58,7 @@ void rmsort(int first, int last, double V[])
 /*
  * Call recursive mergesort.
  */
-void mergesort(int size, double V[])
+void mergesort(size_t size, double V[])
 {
 	rmsort(0, size - 1, V);
 }
@@ -65,9 +66,9 @@ void mergesort(int size, double V[])
 /*
  * Check if the array is sorted.
  */
-int issorted(int size, double V[])
+size_t issorted(size_t size, double V[])
 {
-	for (int t = 0; t < size - 1; ++t) {
+	for (size_t t = 0; t < size - 1; ++t) {
 		if(V[t] > V[t + 1])
 			return 0;
 	}
@@ -76,21 +77,48 @@ int issorted(int size, double V[])
 
 int main(void)
 {
-	double V[] = {4.0, 52.0, 23.5, 12.1, 34.2, 67.8, 90.1, 11.3, 45.6, 78.9,
-        22.2, 33.3, -44.4, 55.5, -66.6, 77.7, -88.8, 99.9, 10.0, 20.1, 31.2, 41.3,
-       	-51.4, -61.5, 71.6, 81.7, 91.8, 2.9, 13.0, 24.1};
-
-	int size = sizeof(V) / sizeof(V[0]);
+	double V[] = {
+		4.0,
+		52.0,
+		23.5,
+		12.1,
+		34.2,
+		67.8,
+		90.1,
+		11.3,
+		45.6,
+		78.9,
+		22.2,
+		33.3,
+		-44.4,
+		55.5,
+		-66.6,
+		77.7,
+		-88.8,
+		99.9,
+		10.0,
+		20.1,
+		31.2,
+		41.3,
+		-51.4,
+		-61.5,
+		71.6,
+		81.7,
+		91.8,
+		2.9,
+		13.0,
+		24.1,
+		};
+	size_t size = sizeof(V) / sizeof(V[0]);
 
 	mergesort(size, V);
 
-	int sorted = issorted(size, V);
-
+	size_t sorted = issorted(size, V);
 	if (sorted) {
-		for (int i = 0; i < size; ++i)
+		for (size_t i = 0; i < size; ++i)
 			printf("%f\n", V[i]);
 	} else {
-		printf("error: the array is not sorted\n");
+		printf("The array is not sorted.\n");
 	}
 	return 0;
 }
